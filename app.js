23dfe6e496be0fdb50d155bf5022fb14c6bb5bcc
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-04-13 07:48:24 +0530

    Handling CORS

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+// Cors
+app.use((req, res, next) => {
+    res.header(
+        'Access-Control-Allow-Origin',
+        '*'  // or add your url to which you want to limit the api usage to
+    );
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS'){
+        res.header(
+            'Access-Control-Allow-Methods',
+            'PUT, POST, PATCH, DELETE, GET'
+        );
+        return res.status(200).json({});
+    }
+});
+
 app.use('/aliens', aliensRoutes);
 
 app.use((req, res, next) => {
