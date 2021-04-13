47090cb2cd16487a8a3616722e5e95d6e0806505
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-04-13 11:03:56 +0530

    name & power required

--- api/models/alien.js
+++ api/models/alien.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose");
 
 const alienSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    power: Number
+    name: {
+        type: String,
+        required: true
+    },
+    power: {
+        type: Number,
+        required: true
+    }
 });
 
 module.exports = mongoose.model('Alien', alienSchema);
