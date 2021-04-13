3f524331da502dc7ff872dcd231fc92ca15dc707
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-04-13 06:23:46 +0530

    Dependencies: morgan

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const aliensRoutes = require('./api/routes/aliens');
+
+app.use(morgan('dev'));
 
 app.use('/aliens', aliensRoutes);
 
