a762dcb860bc91e791ff5ce4e511f2f0f256ff76
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-04-16 22:21:59 +0530

    Improved: DELETE route for user

--- api/routes/user.js
+++ api/routes/user.js
@@ -51,11 +51,11 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.delete('/:userId', (req, res, next) => {
-  User.remove({ _id: req.params.userId })
+  User.deleteOne({ _id: req.params.userId })
     .exec()
     .then(result => {
       res.status(200).json({
-        message: "User : " + email + "Deleted"
+        message: "User Deleted"
       });
     })
     .catch(err => {
