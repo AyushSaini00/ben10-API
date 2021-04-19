2167c0c28646a090f6ae96181abda0705884026b
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-04-19 05:43:44 +0530

    Removed: auth from get request for all aliens & alien by id

--- api/routes/aliens.js
+++ api/routes/aliens.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 const AliensController = require('../controllers/aliens');
 
-router.get('/', checkAuth, AliensController.aliens_get_all);
+// baseURL/aliens
+router.get('/', AliensController.aliens_get_all);
 router.post('/', checkAuth, AliensController.aliens_create_alien);
-router.get("/:alienId", checkAuth, AliensController.aliens_get_by_id);
+
+// baseURL/aliens/alienId
+router.get("/:alienId", AliensController.aliens_get_by_id);
 router.patch('/:alienId', checkAuth, AliensController.aliens_patch_by_id);
 router.delete('/:alienId', checkAuth, AliensController.aliens_delete_by_id);
 
