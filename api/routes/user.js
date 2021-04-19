2e18c09ce849ff02e68cea555817187e455af153
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-04-19 06:20:04 +0530

    bueatify: comments

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const UserController = require('../controllers/user');
 const checkAuth = require('../middleware/check-auth');
 
+// baseURL/user/signup
 router.post('/signup', UserController.user_signup);
+
+// baseURL/user/login
 router.post('/login', UserController.user_login);
+
+// baseURL/user/userId
 router.delete('/:userId', checkAuth, UserController.user_delete);
 
 module.exports = router;
